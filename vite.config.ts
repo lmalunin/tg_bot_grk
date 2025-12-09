070d3bc851c309bb74639e53426f56d5303de5a2
Malunin Lev <lmalunin@gmail.com>
2025-12-09 23:59:07 +0300

    init 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-telegram-app/webapp/',  // Укажите имя репозитория
-})
+  base: "/tg_bot_grk/", // Укажите имя репозитория
+});
